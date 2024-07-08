9cd363f6d0306ff87ea2eb5df09d5d64428242f7
MASINA Elliot <elliot.masina@epitech.eu>
2024-07-08 10:43:18 +0200

    [FEAT]: use webEngine, web page on menu

--- src/Page/mainPage.cpp
+++ src/Page/mainPage.cpp
@@ -1,14 +1,31 @@
 #include <QBoxLayout>
+#include <QtWebEngineWidgets/QWebEngineView>
+#include <QtWebEngineQuick/QtWebEngineQuick>
 
 #include "QtApp/Page/MainPage.hpp"
 #include "QtApp/Tools.hpp"
 
 qta::MainPage::MainPage(const std::string& title) : APage(title)
 {
-    APage::setWidget(new QWidget());
+    APage::setWidget(new QWidget(this));
     APage::setLayout(new QVBoxLayout(APage::getWidget()));
+
     auto *button = new QPushButton("Go to Page 2", APage::getWidget());
     APage::addButton(button, "main");
+
+    QHBoxLayout *webLayout = new QHBoxLayout(APage::getLayout()->widget());
+
+    auto *webEngineView1 = new QWebEngineView(APage::getWidget());
+    webEngineView1->load(QUrl("https://www.youtube.com/"));
+    webEngineView1->setFixedSize(800, 600);
+
+    auto *webEngineView2 = new QWebEngineView(APage::getWidget());
+    webEngineView2->load(QUrl("https://www.chatgpt.com/"));
+    webEngineView2->setFixedSize(800, 600);
+
+    webLayout->addWidget(webEngineView1);
+    webLayout->addWidget(webEngineView2);
+    APage::getLayout()->addLayout(webLayout);
     APage::getLayout()->addStretch(1);
     APage::getLayout()->addWidget(button);
 }
